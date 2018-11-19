0ea3ccadfc0fcb8df7d90964f00ee9b0d813217b
wuxiaobu <woshilieqie@163.com>
2018-11-19 22:40:48 +0800

    CheckIfEmailVerified middleware

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ Route::get('/', 'PagesController@root')->name('root');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['middleware' => 'auth'], function() {
+  Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
+  
+  Route::group(['middleware' => 'email_verified'], function() {
+     Route::get('/test', function() {
+         return 'Your email is verified';
+     });
+ });
+});
