9cdf53303e23f00f00f7619dd051cf639753f787
wuxiaobu <woshilieqie@163.com>
2019-07-14 23:25:49 +0800

    Order Model

---
+++ database/migrations/2019_07_14_142257_create_order_items_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrderItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_items', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('order_id');
+            $table->unsignedBigInteger('product_id');
+            $table->unsignedBigInteger('product_sku_id');
+            $table->unsignedInteger('amount');
+            $table->decimal('price', 10, 2);
+            $table->unsignedInteger('rating')->nullable();
+            $table->text('review')->nullable();
+            $table->timestamp('reviewed_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_items');
+    }
+}
